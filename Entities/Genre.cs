9115d0f8cd065f4df9813359f3968f09ac86f702
Taher Ahmed <Taher.Ahmed@softec.ai>
2025-11-15 07:52:12 +0200

    Create Entities Folder

---
+++ Entities/Genre.cs
@@ -0,0 +1,10 @@
+﻿namespace Building_Minimal_APIs_With_ASPnet_Core_And_EF_Core_Movies_App.Entities
+{
+    public class Genre
+    {   
+        public int Id { get; set; }
+        //public string Name { get; set; } = string.Empty;
+        public string Name { get; set; } = null!;
+
+    }
+}
