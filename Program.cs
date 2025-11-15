1586aa1e5175ea9816df7317110552c7ad9e7a74
Taher Ahmed <Taher.Ahmed@softec.ai>
2025-11-15 07:54:09 +0200

    create && test use Genres endpoint

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Building_Minimal_APIs_With_ASPnet_Core_And_EF_Core_Movies_App.Entities;
 using Microsoft.AspNetCore.Http;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,6 +15,32 @@ var app = builder.Build();
 // Middleware Zone - START
 
 app.MapGet("/", () => ConfigName);
+app.MapGet("/genres", () =>
+{
+    var GenresList = new List<Genre>
+    {
+        new Genre
+        {
+            Id = 1,
+            Name = "Drama"
+        },
+        new Genre
+        {
+            Id = 2,
+            Name = "Action"
+        },
+        new Genre
+        {
+            Id = 3,
+            Name = "Comedy"
+        }
+    };
+
+    return GenresList;
+
+});
+
+
 
 // Middleware Zone - END
 
