e0b19da41281b2e87550e31f6963d9676db177d3
Taher Ahmed <Taher.Ahmed@softec.ai>
2025-11-19 14:31:48 +0200

    Add AutoMapperProfiles class to configure mappings between Genre and DTOs

---
+++ Utilities/AutoMapperProfiles.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Building_MinimalAPIsMoviesApp.DTOs;
+using Building_MinimalAPIsMoviesApp.Entities;
+
+namespace Building_MinimalAPIsMoviesApp.Utilities
+{
+    public class AutoMapperProfiles: Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<Genre, GenreDTO>();
+            CreateMap<CreateGenreDTO, Genre>();
+        }
+    }
+}
