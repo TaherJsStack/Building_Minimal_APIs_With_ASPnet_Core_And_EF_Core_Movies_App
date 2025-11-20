6d4ce0bb7911f2810fc7d329707453e6871a0a40
Taher Ahmed <Taher.Ahmed@softec.ai>
2025-11-20 21:11:30 +0200

    create HttpContextExtensions class

---
+++ Repositories/HttpContextExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Building_MinimalAPIsMoviesApp.Repositories
+{
+    public static class HttpContextExtensions
+    {
+        public async static Task InsertpaginationparametersInResponseHeader<T>
+            ( this HttpContext httpContext, IQueryable<T> queryable ) 
+        { 
+            if ( httpContext is null )
+            {
+                throw new ArgumentNullException(nameof(httpContext));
+            }
+
+            double count = await queryable.CountAsync();
+            httpContext.Response.Headers.Append("TotalAmountOfRecordes", count.ToString() );
+        }
+
+    }
+}
