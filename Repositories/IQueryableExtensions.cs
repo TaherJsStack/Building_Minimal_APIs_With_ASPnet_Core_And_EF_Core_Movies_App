13f890504beaa6fa6b9c7ae4286f94d553d9b50b
Taher Ahmed <Taher.Ahmed@softec.ai>
2025-11-20 21:11:53 +0200

    create IQueryableExtensions class

---
+++ Repositories/IQueryableExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Building_MinimalAPIsMoviesApp.DTOs;
+
+namespace Building_MinimalAPIsMoviesApp.Repositories
+{
+    public static class IQueryableExtensions
+    {
+
+        public static IQueryable<T> Pagination<T>(this IQueryable<T> queryable, PaginationDTO paginationDTO)
+        {
+            return queryable
+                .Skip((paginationDTO.Page - 1) * paginationDTO.RecordesPerPage)
+                .Take(paginationDTO.RecordesPerPage);
+        }
+
+    }
+}
