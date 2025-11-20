0e515a0dd606dda10bc909e179c7b76359ed8bc9
Taher Ahmed <Taher.Ahmed@softec.ai>
2025-11-20 06:00:05 +0200

    add IActorsRepository to Program cs

--- Program.cs
+++ Program.cs
@@ -38,7 +38,8 @@ builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddScoped<IGenresRepositories, GenresRepository>();
+builder.Services.AddScoped<IGenresRepository, GenresRepository>();
+builder.Services.AddScoped<IActorsRepository, ActorsRepository>();
 
 builder.Services.AddAutoMapper(typeof(Program));
 
