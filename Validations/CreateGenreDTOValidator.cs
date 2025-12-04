6b88dc0416ecc1cc456e7a13dd0878604adac2d0
Taher Ahmed <Taher.Ahmed@softec.ai>
2025-12-05 01:24:47 +0200

    Enhance CreateGenreDTOValidator with custom error messages and additional validation rules for genre name

--- Validations/CreateGenreDTOValidator.cs
+++ Validations/CreateGenreDTOValidator.cs
@@ -7,7 +7,25 @@ namespace Building_MinimalAPIsMoviesApp.Validations
     {
         public CreateGenreDTOValidator()
         {
-            RuleFor(p => p.Name).NotEmpty();
+            RuleFor(p => p.Name)
+                .NotEmpty()
+                .WithMessage("Custom Not Empty Message Error For {PropertyName}")
+                .MaximumLength(150)
+                .WithMessage("Custom Maximum Length Message Error For {PropertyName}")
+                .Must(FirstLitterIsUppercase)
+                .WithMessage("First Litter Is Uppercase Error Message")
+                ;
+        }
+
+        private bool FirstLitterIsUppercase(string value) 
+        { 
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return true;
+            };
+
+            var firestLitter = value[0].ToString();
+            return firestLitter == firestLitter.ToUpper();
         }
     }
 }
