6f522bd2483ca40c93d8f30f20836384f30e45fa
Taher Ahmed <Taher.Ahmed@softec.ai>
2025-12-04 07:13:44 +0200

    Add ActorsMovies collection to Movie entity to establish relationships with actors

--- Entities/Movie.cs
+++ Entities/Movie.cs
@@ -8,8 +8,8 @@
         public DateTime? ReleaseDate { get; set; }
         public string? Poster { get; set; }
         public List<Comment> Comments { get; set; } = new List<Comment>();
-
         public List<GenreMovie> GenresMovies { get; set; } = new List<GenreMovie>();
+        public List<ActorMovie> ActorsMovies { get; set; } = new List<ActorMovie>();
 
     }
 }
