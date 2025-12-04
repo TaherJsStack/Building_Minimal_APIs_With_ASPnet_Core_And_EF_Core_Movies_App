26f15392f82f14db820e3d2c7ce2ee19f88c70b3
Taher Ahmed <Taher.Ahmed@softec.ai>
2025-12-05 01:08:10 +0200

    Add CreateGenreDTOValidator for genre validation using FluentValidation

---
+++ Validations/CreateGenreDTOValidator.cs
@@ -0,0 +1,13 @@
+﻿using Building_MinimalAPIsMoviesApp.DTOs;
+using FluentValidation;
+
+namespace Building_MinimalAPIsMoviesApp.Validations
+{
+    public class CreateGenreDTOValidator: AbstractValidator<CreateGenreDTO>
+    {
+        public CreateGenreDTOValidator()
+        {
+            RuleFor(p => p.Name).NotEmpty();
+        }
+    }
+}
