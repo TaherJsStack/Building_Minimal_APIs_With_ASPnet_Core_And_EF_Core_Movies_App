aea5519614313acddf6cdb2f38b1cb6f0c8ef90f
Taher Ahmed <Taher.Ahmed@softec.ai>
2025-12-05 15:20:25 +0200

    Add parameters to TestFilter for enhanced request processing, enabling access to int, IGenresRepository, and IMapper within the filter context.

--- Filters/TestFilter.cs
+++ Filters/TestFilter.cs
@@ -1,10 +1,17 @@
 ﻿
+using AutoMapper;
+using Building_MinimalAPIsMoviesApp.Repositories;
+
 namespace Building_MinimalAPIsMoviesApp.Filters
 {
     public class TestFilter : IEndpointFilter
     {
         public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
         {
+            var param1 = context.Arguments.OfType<int>().FirstOrDefault();
+            var param2 = context.Arguments.OfType<IGenresRepository>().FirstOrDefault();
+            var param3 = context.Arguments.OfType<IMapper>().FirstOrDefault();
+
             //this is the code that will execute Before the endpoint
             var results = await next(context);
             //this is the code that will execute After the endpoint
