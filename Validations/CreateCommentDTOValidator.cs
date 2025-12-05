3063d956a7ce6426f87c542c1a9220b4c9cb57e1
Taher Ahmed <Taher.Ahmed@softec.ai>
2025-12-05 17:00:52 +0200

    Add CreateCommentDTOValidator for comment validation, ensuring non-empty and length-restricted comment bodies for improved request validation.

---
+++ Validations/CreateCommentDTOValidator.cs
@@ -0,0 +1,18 @@
+﻿using Building_MinimalAPIsMoviesApp.DTOs;
+using FluentValidation;
+
+namespace Building_MinimalAPIsMoviesApp.Validations
+{
+    public class CreateCommentDTOValidator: AbstractValidator<CreateCommentDTO>
+    {
+        public CreateCommentDTOValidator() 
+        {
+            RuleFor(p => p.Body)
+                .NotEmpty()
+                .WithMessage(ValidationUtilities.NotEmptyMessage)
+                .MaximumLength(350)
+                .WithMessage(ValidationUtilities.MaximumLengthMessage);
+
+        }
+    }
+}
