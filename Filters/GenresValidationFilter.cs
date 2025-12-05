0b5aeca3e8182442bcc11516b2077f52e0af3603
Taher Ahmed <Taher.Ahmed@softec.ai>
2025-12-05 16:37:10 +0200

    Add GenresValidationFilter to implement endpoint validation for CreateGenreDTO, ensuring proper validation and error handling for genre creation requests.

---
+++ Filters/GenresValidationFilter.cs
@@ -0,0 +1,36 @@
+﻿
+using AutoMapper;
+using Building_MinimalAPIsMoviesApp.DTOs;
+using Building_MinimalAPIsMoviesApp.Repositories;
+using FluentValidation;
+
+namespace Building_MinimalAPIsMoviesApp.Filters
+{
+    public class GenresValidationFilter : IEndpointFilter
+    {
+        public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
+        {
+            var validator = context.HttpContext.RequestServices.GetService<IValidator<CreateGenreDTO>>();
+
+            if (validator is null)
+            {
+                return await next(context);
+            }
+
+            var obj = context.Arguments.OfType<CreateGenreDTO>().FirstOrDefault();
+
+            if (obj is null)
+            {
+                return Results.Problem("the object to validate canot be found. ");
+            }
+
+            var validationResult = await validator.ValidateAsync(obj);
+            if (!validationResult.IsValid)
+            {
+                return Results.ValidationProblem(validationResult.ToDictionary());
+            }
+
+            return await next(context); ;
+        }
+    }
+}
